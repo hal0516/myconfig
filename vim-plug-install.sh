83ca4599937683befb279000372a09b4da9cd6fa
hal0516 <78209294+hal0516@users.noreply.github.com>
2023-01-04 10:56:14 +0800

    Update vim-plug-install.sh

--- vim-plug-install.sh
+++ vim-plug-install.sh
@@ -1,2 +1,2 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim/autoload/
+cp ./plug.vim ~/.vim/autoload/
